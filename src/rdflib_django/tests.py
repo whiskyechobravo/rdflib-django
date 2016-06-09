b2e61069dc74fde8a2f2aec7334040618fd138a6
Anthony Lalande <anthony.lalande@gmail.com>
2016-06-09 17:15:30 -0400

    Include doctests via the `load_tests` protocol.

--- src/rdflib_django/tests.py
+++ src/rdflib_django/tests.py
@@ -4,18 +4,13 @@ Unittests and doctests for the rdflib_django app.
 import doctest
 import rdflib_django
 import unittest
-from rdflib_django import store, test_store, test_rdflib, test_seq, test_namespaces
+from rdflib_django import store
 
 
-def suite():
+def load_tests(loader, tests, ignore):
     """
     Generate the test suite.
     """
-    s = unittest.TestSuite()
-    s.addTest(doctest.DocTestSuite(rdflib_django))
-    s.addTest(doctest.DocTestSuite(store))
-    s.addTest(unittest.findTestCases(test_store))  # pylint: disable=no-member
-    s.addTest(unittest.findTestCases(test_rdflib))  # pylint: disable=no-member
-    s.addTest(unittest.findTestCases(test_seq))  # pylint: disable=no-member
-    s.addTest(unittest.findTestCases(test_namespaces))  # pylint: disable=no-member
-    return s
+    tests.addTest(doctest.DocTestSuite(rdflib_django))
+    tests.addTest(doctest.DocTestSuite(store))
+    return tests
